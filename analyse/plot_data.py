703b27d9f9cf370f6e2200f0ae63639702411f5e
Thomas <thomas.traber@gmx.net>
2016-06-24 08:57:16 +0200

    Raw Data Plotter added

---
+++ analyse/plot_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+#vim: fileencoding=utf8
+"""
+Plots the raw data from the MPU logger file
+"""
+
+from pylab import *
+import sys
+import os.path
+
+def plotraw(filename):
+    basename=os.path.splitext(filename)[0]
+    D = loadtxt(filename,comments="#")
+    times = D[:,0]*1e-4
+    times -= times[0]
+    gx = D[:,1]
+    gy = D[:,2]
+    gz = D[:,3]
+    try:
+        ax = D[:,4]
+        ay = D[:,5]
+        az = D[:,6]
+    except:
+        print "No accelerator data available"
+
+    grid(1)    
+    plot(times,gx,label="gx")
+    plot(times,gy,label="gy")
+    plot(times,gz,label="gz")
+    legend(loc=2)
+    ylabel("Angular Speed")
+    xlabel("Time / s")
+    twinx()
+    plot(times,ax,ls=":",label="ax")
+    plot(times,ay,ls=":",label="ay")
+    plot(times,az,ls=":",label="az")
+    legend(loc=1)
+    ylabel("Acceleration")
+    savefig("%s.png"%basename)
+
+if __name__=="__main__":
+    plotraw(sys.argv[1])
